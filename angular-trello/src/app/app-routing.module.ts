a8f3dde31fef7578583d18817aa71ad61c9b00e9
AjithRithik <AjithRithik@AM.github.com>
2019-09-28 00:39:01 +0530

    initial commit. project created and add the required dependencies

---
+++ angular-trello/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { WelcomeBoardComponent } from './welcome-board/welcome-board.component';
+
+
+const routes: Routes = [
+  { path:'dashboard', component: DashBoardComponent },
+  { path:'welcome-board', component: WelcomeBoardComponent },
+  { path:'', redirectTo:'/dashboard', pathMatch:'full' },
+  { path:'**', redirectTo:'/dashboard', pathMatch:'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ angular-trello/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { WelcomeBoardComponent } from './welcome-board/welcome-board.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashBoardComponent,
+    WelcomeBoardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
